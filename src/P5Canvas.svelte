47b30efe014c081823939b0f3f10f758b8a482d4
Nafis Fuad <fuadbd14@gmail.com>
2019-12-31 23:57:47 +0600

    Add p5 canvas component

---
+++ src/P5Canvas.svelte
@@ -0,0 +1,13 @@
+<script>
+	import p5 from "p5";
+	import { onMount } from "svelte";
+
+	onMount(function() {
+	  let myp5 = new p5(sketch, id);
+	});
+
+	export let id;
+	export let sketch;
+</script>
+
+<div {id}></div>
