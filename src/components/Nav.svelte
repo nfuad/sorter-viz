e3a79d36adda1e15807983bef130fee239547678
Nafis Fuad <fuadbd14@gmail.com>
2020-01-01 02:49:02 +0600

    Add Nav component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,18 @@
+<style>
+  nav {
+    position: fixed;
+    top: 15px;
+    left: 0;
+    width: 100%;
+    display: block;
+  }
+
+  p {
+    max-width: 150px;
+    margin-left: 150px;
+  }
+</style>
+
+<nav>
+  <p>Choose an option:</p>
+</nav>
