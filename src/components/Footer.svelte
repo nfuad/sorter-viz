764f0c3e564d1ff15c615289703257ff272ac6fc
Nafis Fuad <fuadbd14@gmail.com>
2020-01-01 02:49:26 +0600

    Update formatting

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,25 +1,26 @@
 <footer>
-<p>Created by
-  <a href="https://nafis.co" target="_blank">Nafis Fuad</a> with
-  <a href="https://svelte.dev/" target="_blank">Svelte</a> &amp;
-  <a href="https://p5js.org" target="_blank">P5.js</a>. Find the source code
-  <a href="https://github.com/nfuad">here on GitHub</a>.
-  {new Date().getFullYear()}, No rights reserved. Feel free to copy me 😉</p>
+  <p>
+    Created by <a href="https://nafis.co" target="_blank">Nafis Fuad</a> with
+    <a href="https://svelte.dev/" target="_blank">Svelte</a> &amp;
+    <a href="https://p5js.org" target="_blank">P5.js</a>. Find the source code
+    <a href="https://github.com/nfuad">here on GitHub</a>. {new
+    Date().getFullYear()}, No rights reserved. Feel free to copy me 😉
+  </p>
 </footer>
 
 <style>
-footer{
-  width: 100%;
-  text-align: center;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  padding: 25px 0px;
-}
+  footer {
+    width: 100%;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 25px 0px;
+  }
 
-p{
-  width: 40%;
-  margin: 0 auto;
-}
+  p {
+    width: 40%;
+    margin: 0 auto;
+  }
 </style>
--- src/components/P5Canvas.svelte
+++ src/components/P5Canvas.svelte
@@ -1,13 +1,13 @@
 <script>
-	import p5 from "p5";
-	import { onMount } from "svelte";
+  import p5 from "p5";
+  import { onMount } from "svelte";
 
-	onMount(function() {
-	  let myp5 = new p5(sketch, id);
-	});
+  onMount(function() {
+    let myp5 = new p5(sketch, id);
+  });
 
-	export let id;
-	export let sketch;
+  export let id;
+  export let sketch;
 </script>
 
 <div {id}></div>
