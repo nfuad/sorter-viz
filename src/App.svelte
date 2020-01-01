291675a484aa722f7751f63a07e2d7e116799d52
Nafis Fuad <fuadbd14@gmail.com>
2020-01-01 23:41:42 +0600

    Add components

--- src/App.svelte
+++ src/App.svelte
@@ -1,107 +1,23 @@
 <script>
-  import GHCorner from "./components/GHCorner.svelte";
-  import P5Canvas from "./components/P5Canvas.svelte";
   import Footer from "./components/Footer.svelte";
-  import Nav from './components/Nav.svelte'
-
-  import {
-    bogoSort,
-    bubbleSort,
-    combSort,
-    heapSort,
-    insertionSort,
-    mergeSort,
-    quickSortHoare,
-    quickSortLomuto,
-    radixSortLSD,
-    selectionSort,
-    shellSort
-  } from "./algorithms";
-
-  const sketch = p5 => {
-    const CONTROL_HEIGHT = 20;
-    const TILE_WIDTH = 20;
-    const DEFAULT_ALGORITHM = "Merge Sort";
-    const SORT_CLASSES = {
-      "Bubble Sort": bubbleSort,
-      "Insertion Sort": insertionSort,
-      "Selection Sort": selectionSort,
-      "Quick Sort (hoare)": quickSortHoare,
-      "Quick Sort (lomuto)": quickSortLomuto,
-      "Bogo Sort": bogoSort,
-      "Merge Sort": mergeSort,
-      "Heap Sort": heapSort,
-      "Shell Sort": shellSort,
-      "Radix Sort (LSD)": radixSortLSD,
-      "Comb Sort": combSort
-    };
-
-    let N;
-    let M;
-    let values;
-    let sorters;
-    let sortersFinished;
-    let select;
-
-    p5.setup = () => {
-      p5.createCanvas(p5.windowWidth / 1.4, p5.windowHeight / 1.4);
-      p5.colorMode(p5.HSL, 360, 100, 100);
-      p5.frameRate(60);
-
-      setupUI(DEFAULT_ALGORITHM);
-
-      N = Math.floor(p5.width / TILE_WIDTH);
-      M = Math.floor(p5.height / TILE_WIDTH);
-
-      values = new Array(M);
-      sorters = new Array(M);
-
-      startSorting(DEFAULT_ALGORITHM);
-    };
-
-    const setupUI = initialValue => {
-      select = p5.createSelect();
-      Object.keys(SORT_CLASSES).forEach(k => {
-        select.option(k);
-      });
-      select.value(initialValue);
-      select.changed(() => startSorting(select.value()));
-    };
-
-    const startSorting = algorithm => {
-      values = new Array(M);
-      sorters = new Array(M);
-      sortersFinished = new Array(M);
-
-      for (let i = 0; i < M; i++) {
-        values[i] = new Array(N);
-        for (let j = 0; j < N; j++) {
-          values[i][j] = j;
-        }
-        p5.shuffle(values[i], true);
-        sorters[i] = SORT_CLASSES[algorithm](values[i]);
-        sortersFinished[i] = false;
-      }
-    };
-
-    p5.draw = () => {
-      for (let i = 0; i < M; i++) {
-        if (sorters[i].next().done) {
-          sortersFinished[i] = true;
-          continue;
-        }
-        for (let j = 0; j < N; j++) {
-          let c = p5.color(p5.map(values[i][j], 0, N, 0, 360), 100, 50);
-          p5.stroke(c);
-          p5.fill(c);
-          p5.rect(j * TILE_WIDTH, i * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
-        }
-      }
-    };
-  };
+  import Nav from "./components/Nav.svelte";
+  import Visualizer1 from "./components/visualizers/Visualizer1.svelte";
+  import Visualizer2 from "./components/visualizers/Visualizer2.svelte";
 </script>
 
-<Nav />
-<GHCorner />
-<P5Canvas sketch="{sketch}" />
-<Footer />
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    width: 100%;
+  }
+</style>
+
+<div class="wrapper">
+  <Nav />
+  <Visualizer1 />
+  <Visualizer2 />
+  <Footer />
+</div>
