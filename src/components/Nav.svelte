d517f76133794564ba3ef32f38ae0718a8a3b0e0
Nafis Fuad <fuadbd14@gmail.com>
2020-01-01 23:43:10 +0600

    Update Nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,18 +1,61 @@
+<script>
+  import GHCorner from "./GHCorner.svelte";
+
+  const SORT_CLASSES = [
+    { name: "Bubble Sort", algo: "bubbleSort" },
+    { name: "Insertion Sort", algo: "insertionSort" },
+    { name: "Selection Sort", algo: "selectionSort" },
+    { name: "Quick Sort (hoare)", algo: "quickSortHoare" },
+    { name: "Quick Sort (lomuto)", algo: "quickSortLomuto" },
+    { name: "Bogo Sort", algo: "bogoSort" },
+    { name: "Merge Sort", algo: "mergeSort" },
+    { name: "Heap Sort", algo: "heapSort" },
+    { name: "Shell Sort", algo: "shellSort" },
+    { name: "Radix Sort (LSD)", algo: "radixSortLSD" },
+    { name: "Comb Sort", algo: "combSort" }
+  ];
+
+  let selected;
+</script>
+
 <style>
   nav {
-    position: fixed;
-    top: 15px;
-    left: 0;
     width: 100%;
-    display: block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0;
+    position: sticky;
+    top: 0;
+    left: 0;
+    background: #333;
+    font-size: 15px;
   }
 
   p {
-    max-width: 150px;
-    margin-left: 150px;
+    margin-right: 20px;
   }
 </style>
 
 <nav>
-  <p>Choose an option:</p>
+  <GHCorner />
+
+  <p>Choose A Sorting Algorithm:</p>
+  <select
+    bind:value={selected}
+    on:change={() => {
+      console.log(selected);
+      const SELECT_EVENT = new CustomEvent('SELECT_EVENT', {
+        detail: { selected }
+      });
+      window.dispatchEvent(SELECT_EVENT);
+    }}>
+    {#each SORT_CLASSES as sort_class}
+      <option
+        value={sort_class.algo}
+        selected={sort_class.algo === 'mergeSort'}>
+        {sort_class.name}
+      </option>
+    {/each}
+  </select>
 </nav>
